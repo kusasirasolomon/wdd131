a6afd0e75d69a404cf575ef310d3bdcce0a78b1e
kusasirasolomon <skusasira@byupathway.edu>
2025-07-08 18:40:27 -0700

    i have made home page in my real respiretory

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+// scripts/getdates.js
+
+// Current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Last modified date
+document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
